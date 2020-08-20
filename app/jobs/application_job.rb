1efc6843fb767ea75cff79211c0bf932004f11a5
Kylie <chiixil.84@gmail.com>
2020-08-20 18:05:52 -0500

    messed with some of this lol, but I need to update more

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,2 +1,4 @@
 class ApplicationJob < ActiveJob::Base
 end
+
+# basically whenever you make a new status on discord, when it asks 'when do you want to delete this??', this code will do that for you
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,3 +2,6 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
 end
+
+
+# user confirmation 'thank you for your purchase!! :)', etc
